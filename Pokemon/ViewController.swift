596d0bcae2f3acb3b092f6b3ec405c4772956dd6
Jaime Uribe <jauribe@intergrupo.com>
2021-01-23 11:37:31 -0500

    Initial Commit

---
+++ Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Jaime Uribe on 23/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
